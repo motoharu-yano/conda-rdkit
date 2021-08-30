2100ea027370438c8d33bee39f96aa63dc8e3f92
motoharu-yano <muzhedgehog@list.ru>
2021-08-30 18:47:27 +0300

    change meta.yaml based on conda-forge recipe. adjust build scripts. drag python into runtime dependencies. use conda-forge postgresql and postgresql-plpython version instead of default version.

--- rdkit-postgresql/bld.bat
+++ rdkit-postgresql/bld.bat
@@ -1,35 +1,47 @@
-%PYTHON% "%RECIPE_DIR%\pkg_version.py"
+echo cleaning up previous build
 
-cd "%SRC_DIR%"
-
-rmdir /s /q build 2> NUL
+rmdir /s /q .\build 2> NUL
 mkdir build
 cd build
-:: in case there are any old psql builds: remove them
-rmdir /s /q Code\PgSQL 2> NUL
+
+echo configuring with cmake
+
+rem there is a problem where you can not use 'NMake Makefiles JOM' generator.
+rem this creates incorrect pgsql_install.bat file at work\build\Code\PgSQL\rdkit where last line that installs rdkit.dll is incorrect
+rem it contains 'Release' folder in the path like work\build\Code\PgSQL\rdkit\Release\rdkit.dll
+rem while binary is still put into work\build\Code\PgSQL\rdkit\rdkit.dll
+rem so the binary can not be copied and since installation is not properly done - postgres load test for rdkit cartridge fails
 
 cmake ^
     -G "NMake Makefiles" ^
-    -D RDK_BUILD_PGSQL=ON ^
-    -D RDK_PGSQL_STATIC=ON ^
-    -D RDK_INSTALL_INTREE=OFF ^
+    -D CMAKE_SYSTEM_PREFIX_PATH=%LIBRARY_PREFIX% ^
+    -D CMAKE_BUILD_TYPE=Release ^
+    -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -D BOOST_ROOT=%LIBRARY_PREFIX% ^
+    -D Boost_NO_SYSTEM_PATHS=ON ^
+    -D Boost_NO_BOOST_CMAKE=ON ^
+    -D RDK_BUILD_AVALON_SUPPORT=ON ^
+    -D RDK_BUILD_CPP_TESTS=OFF ^
+    -D RDK_BUILD_INCHI_SUPPORT=ON ^
     -D RDK_INSTALL_STATIC_LIBS=OFF ^
     -D RDK_INSTALL_DEV_COMPONENT=OFF ^
-    -D RDK_BUILD_INCHI_SUPPORT=ON ^
-    -D RDK_BUILD_AVALON_SUPPORT=ON ^
+    -D RDK_INSTALL_INTREE=OFF ^
+    -D RDK_BUILD_PYTHON_WRAPPERS=OFF ^
+    -D RDK_USE_BOOST_SERIALIZATION=OFF ^
     -D RDK_BUILD_FREESASA_SUPPORT=OFF ^
     -D RDK_USE_FLEXBISON=OFF ^
     -D RDK_BUILD_THREADSAFE_SSS=ON ^
     -D RDK_TEST_MULTITHREADED=ON ^
     -D RDK_OPTIMIZE_POPCNT=ON ^
-    -D RDK_BUILD_CPP_TESTS=OFF ^
-    -D RDK_BUILD_PYTHON_WRAPPERS=OFF ^
-    -D RDK_USE_BOOST_SERIALIZATION=OFF ^
-    -D CMAKE_SYSTEM_PREFIX_PATH=%LIBRARY_PREFIX% ^
-    -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
-    -D BOOST_ROOT=%PREFIX% -D Boost_NO_SYSTEM_PATHS=ON ^
-    -D CMAKE_BUILD_TYPE=Release ^
+    -D RDK_BUILD_PGSQL=ON ^
+    -D RDK_PGSQL_STATIC=ON ^
     ..
+
+if errorlevel 1 exit 1
+
+echo starting build
+
+rem building with jom like this because building this recipe with cmake does not seem to parallelize
 
 where jom 2> NUL
 if %ERRORLEVEL% equ 0 (
@@ -40,11 +52,16 @@ if %ERRORLEVEL% equ 0 (
 
 %MAKE_CMD%
 
-cd Code\PgSQL\rdkit
+echo finished build, installing postgres rdkit extension
+
+cd .\Code\PgSQL\rdkit
 call pgsql_install.bat
 
 set PGPORT=54321
 set PGDATA=%SRC_DIR%\pgdata
+
+rem cleanup required when building variants
+rmdir /s /q $PGDATA 2> NULL 
 
 pg_ctl initdb
 
@@ -55,6 +72,8 @@ pg_ctl -D %PGDATA% -l %PGDATA%/log.txt start
 
 rem wait a few seconds just to make sure that the server has started
 ping -n 5 127.0.0.1 > NUL
+
+echo starting the test
 
 set "RDBASE=%SRC_DIR%"
 ctest -V
--- rdkit-postgresql/build.sh
+++ rdkit-postgresql/build.sh
@@ -1,40 +1,42 @@
-mkdir -p build
+rm build -rf
+mkdir build
 cd build
-# in case there are any old psql builds: remove them
-rm -rf Code/PgSQL
 
 cmake \
-    -D RDK_BUILD_PGSQL=ON \
-    -D RDK_PGSQL_STATIC=ON \
-    -D RDK_INSTALL_STATIC_LIBS=ON \
-    -D RDK_INSTALL_INTREE=OFF \
+    -D CMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D CMAKE_INSTALL_PREFIX=$PREFIX \
+    -D BOOST_ROOT=$PREFIX \
+    -D Boost_NO_SYSTEM_PATHS=ON \
+    -D Boost_NO_BOOST_CMAKE=ON \
+    -D RDK_BUILD_AVALON_SUPPORT=ON \
+    -D RDK_BUILD_CPP_TESTS=OFF \
+    -D RDK_BUILD_INCHI_SUPPORT=ON \
     -D RDK_INSTALL_STATIC_LIBS=OFF \
     -D RDK_INSTALL_DEV_COMPONENT=OFF \
-    -D RDK_BUILD_INCHI_SUPPORT=ON \
-    -D RDK_BUILD_AVALON_SUPPORT=ON \
+    -D RDK_INSTALL_INTREE=OFF \
+    -D RDK_BUILD_PYTHON_WRAPPERS=OFF \
+    -D RDK_USE_BOOST_SERIALIZATION=OFF \
     -D RDK_BUILD_FREESASA_SUPPORT=OFF \
     -D RDK_USE_FLEXBISON=OFF \
     -D RDK_BUILD_THREADSAFE_SSS=ON \
     -D RDK_TEST_MULTITHREADED=ON \
-    -D RDK_BUILD_CPP_TESTS=OFF \
-    -D RDK_BUILD_PYTHON_WRAPPERS=OFF \
-    -D RDK_USE_BOOST_SERIALIZATION=OFF \
-    -D CMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
-    -D CMAKE_INSTALL_PREFIX=$PREFIX \
-    -D BOOST_ROOT=$PREFIX -D Boost_NO_SYSTEM_PATHS=ON \
-    -D CMAKE_BUILD_TYPE=Release \
+    -D RDK_OPTIMIZE_POPCNT=ON \
+    -D RDK_BUILD_PGSQL=ON \
+    -D RDK_PGSQL_STATIC=ON \
     ..
 
 make -j$CPU_COUNT
 
 cd ./Code/PgSQL/rdkit
-
 /bin/bash -e ./pgsql_install.sh
 
 export PGPORT=54321
 export PGDATA=$SRC_DIR/pgdata
 
-rm -rf $PGDATA # cleanup required when building variants
+# cleanup required when building variants
+rm -rf $PGDATA
+
 pg_ctl initdb
 
 # ensure that the rdkit extension is loaded at process startup
