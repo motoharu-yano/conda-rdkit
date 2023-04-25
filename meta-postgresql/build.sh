594feac943f706ff931e0ca525d04d41caa49a93
motoharu-yano <muzhedgehog@list.ru>
2023-04-25 18:53:20 +0200

    add pgvector to meta package. bump version to 6.

--- meta-postgresql/build.sh
+++ meta-postgresql/build.sh
@@ -1,7 +1,9 @@
 cp ${RECIPE_DIR}/install_pgsql_gzip_postgresql.sh ${SRC_DIR}/install_pgsql_gzip_postgresql.sh
 cp ${RECIPE_DIR}/install_rdkit_postgresql.sh ${SRC_DIR}/install_rdkit_postgresql.sh
 cp ${RECIPE_DIR}/install_citus.sh ${SRC_DIR}/install_citus.sh
+cp ${RECIPE_DIR}/install_pgvector_postgresql.sh ${SRC_DIR}/install_pgvector_postgresql.sh
 
 sh ./install_pgsql_gzip_postgresql.sh
 sh ./install_rdkit_postgresql.sh
 sh ./install_citus.sh
+sh ./install_pgvector_postgresql.sh
---
+++ meta-postgresql/install_pgvector_postgresql.sh
@@ -0,0 +1,34 @@
+cd pgvector_postgresql_src
+
+make -j$CPU_COUNT
+
+make install
+
+export PGPORT=54322
+export PGDATA=$SRC_DIR/pgdata
+
+# cleanup required when building variants
+rm -rf $PGDATA
+
+initdb -D $PGDATA
+
+# ensure that the gzip extension is loaded at process startup
+echo "shared_preload_libraries = 'vector'" >> $PGDATA/postgresql.conf
+
+sed -i "s/#port = 5432/port = $PGPORT/g" $PGDATA/postgresql.conf
+
+pg_ctl -D $PGDATA -l $PGDATA/log.txt start
+
+# wait a few seconds just to make sure that the server has started
+sleep 2
+
+set +e
+make installcheck        # regression tests
+check_result1=$?
+make prove_installcheck
+check_result12=$?
+set -e
+
+pg_ctl -D $PGDATA stop
+
+exit $check_result1 || $check_result2
